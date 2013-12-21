d7466347df34d2a09bcaabdb7ec90904cab45f96
Marcos Peña <devilcius@gmail.com>
2013-12-21 23:12:05 +0100

    fix: some psr style and translations

--- Lib/Cuaderno19.php
+++ Lib/Cuaderno19.php
@@ -7,282 +7,259 @@ class Cuaderno19
 
     private $presenter = array(
         "nif" => "",
-        "sufijo" => "",
-        "nombre" => "",
-        "entidad" => "",
-        "oficina" => "",
+        "suffix" => "",
+        "name" => "",
+        "bank" => "",
+        "branch" => "",
         "total" => 0,
-        "nOrd" => 0,
-        "nRec" => 0,
-        "nReg" => 0
+        "ordNumber" => 0,
+        "recNumber" => 0,
+        "regNumber" => 0
     );
     private $applicants = array();
     private $individual = array();
 
-    /*
-      Funcion:
-      configuraPresentador(nif,sufijo,nombre,entidad,oficina)
-
-      Parametros:
-      nif: NIF del presentador
-      sufijo: SUFIJO del presentador
-      nombre: NOMBRE del presentador
-      entidad: ENTIDAD BANCARIA del presentador
-      oficina: OFICINA BANCARIA del presentador
-
-      Devuelve:
-      0: El ID del presentador ya que solo se permite un presentador por remesa
+    /**
+     * 
+     * @param type $nif presenter nif
+     * @param type $suffix presenter suffix
+     * @param type $name presenter name
+     * @param type $bank presenter bank
+     * @param type $branch presenter bank branch
+     * @return int presentor id (only one presentor by remittance)
      */
-
-    function configuraPresentador($nif, $sufijo, $nombre, $entidad, $oficina)
+    public function configurePresentator($nif, $suffix, $name, $bank, $branch)
     {
         $this->presenter["nif"] = strtoupper($nif);
-        $this->presenter["sufijo"] = strtoupper($sufijo);
-        $this->presenter["nombre"] = strtoupper($nombre);
-        $this->presenter["entidad"] = strtoupper($entidad);
-        $this->presenter["oficina"] = strtoupper($oficina);
+        $this->presenter["suffix"] = strtoupper($suffix);
+        $this->presenter["name"] = strtoupper($name);
+        $this->presenter["bank"] = strtoupper($bank);
+        $this->presenter["branch"] = strtoupper($branch);
         $this->presenter["total"] = 0;
-        $this->presenter["nOrd"] = 0;
-        $this->presenter["nRec"] = 0;
-        $this->presenter["nReg"] = 2;
+        $this->presenter["ordNumber"] = 0;
+        $this->presenter["recNumber"] = 0;
+        $this->presenter["regNumber"] = 2;
 
         return 0;
     }
 
-    /*
-      Funcion:
-      agregaOrdenante($nif,$sufijo,$nombre,$entidad,$oficina,$dc,$cuenta)
-
-      Parametros:
-      nif: NIF del ordenante
-      sufijo: SUFIJO del ordenante
-      nombre: NOMBRE del ordenante
-      entidad: ENTIDAD BANCARIA del ordenante
-      oficina: OFICINA BANCARIA del ordenante
-      dc: DIGITO DE CONTROL DE LA CUENTA BANCARIA del ordenante
-      cuenta: NUMERO DE CUENTA BANCARIA del ordenante
-
-      Devuelve:
-      entero: El ID del ordenante generado
+    /**
+     * 
+     * @param type $nif appliccant nif
+     * @param type $suffix applicant suffix
+     * @param type $name applicant name
+     * @param type $bank applicant bank
+     * @param type $branch applicant bank branch
+     * @param type $cd applicant account control digit
+     * @param type $account applicant account number (CCC)
+     * @return int applicant generated id
      */
-
-    function agregaOrdenante($nif, $sufijo, $nombre, $entidad, $oficina, $dc, $cuenta)
+    public function addApplicant($nif, $suffix, $name, $bank, $branch, $cd, $account)
     {
         $this->applicants[] = array(
             "nif" => strtoupper($nif),
-            "sufijo" => strtoupper($sufijo),
-            "nombre" => strtoupper($nombre),
-            "entidad" => strtoupper($entidad),
-            "oficina" => strtoupper($oficina),
-            "dc" => strtoupper($dc),
-            "cuenta" => strtoupper($cuenta),
+            "suffix" => strtoupper($suffix),
+            "name" => strtoupper($name),
+            "bank" => strtoupper($bank),
+            "branch" => strtoupper($branch),
+            "cd" => strtoupper($cd),
+            "account" => strtoupper($account),
             "total" => 0,
-            "nRec" => 0,
-            "nReg" => 2
+            "recNumber" => 0,
+            "regNumber" => 2
         );
 
         return count($this->applicants) - 1;
     }
 
-    /*
-      Funcion:
-      agregaRecibo($idOrdenante,$ref,$nombre,$entidad,$oficina,$dc,$cuenta,$importe,$concepto)
-
-      Parametros:
-      idOrdenante: ID DEL ORDENANTE al que le asignamos el recibo agregado
-      ref: REFERENCIA del cliente
-      nombre: NOMBRE del cliente
-      entidad: ENTIDAD BANCARIA del cliente
-      oficina: OFICINA BANCARIA del cliente
-      dc: DIGITO DE CONTROL DE LA CUENTA BANCARIA del cliente
-      cuenta: NUMERO DE CUENTA BANCARIA del cliente
-      importe: IMPORTE del recibo
-      concepto: CONCEPTO del recibo
-
-      Devuelve:
-      entero: El ID del recibo
+    /**
+     * 
+     * @param int $applicantId
+     * @param string $reference debtor reference
+     * @param type $name debtor name
+     * @param type $bank debtor bank
+     * @param type $branch debtor bank branch
+     * @param type $cd debtor control digit
+     * @param type $account debtor account
+     * @param type $amount invoice amount
+     * @param type $concept invoice concept
+     * @return int receipt ID
      */
-
-    function agregaRecibo($idOrdenante, $ref, $nombre, $entidad, $oficina, $dc, $cuenta, $importe, $concepto)
+    public function addReceipt($applicantId, $reference, $name, $bank, $branch, $cd, $account, $amount, $concept)
     {
-        $this->individual[$idOrdenante][] = array(
-            "ref" => strtoupper($ref),
-            "nombre" => strtoupper($nombre),
-            "entidad" => strtoupper($entidad),
-            "oficina" => strtoupper($oficina),
-            "dc" => strtoupper($dc),
-            "cuenta" => strtoupper($cuenta),
-            "importe" => $importe,
-            "concepto" => strtoupper($concepto)
+        $this->individual[$applicantId][] = array(
+            "ref" => strtoupper($reference),
+            "name" => strtoupper($name),
+            "bank" => strtoupper($bank),
+            "branch" => strtoupper($branch),
+            "cd" => strtoupper($cd),
+            "account" => strtoupper($account),
+            "amount" => $amount,
+            "concept" => strtoupper($concept)
         );
 
         return count($this->individual) - 1;
     }
 
-    /*
-      Funcion:
-      generaRemesa()
-
-      Parametros:
-      Ninguno
-
-      Devuelve:
-      cadena de texto con el fichero del cuaderno 19
+    /**
+     * 
+     * @return string (cuaderno 19)
      */
-
-    function generaRemesa()
+    public function generateRemittance()
     {
-        $remesa = "";
+        $remittance = "";
 
-        $remesa.= $this->_generaPresentador($this->presenter["nif"], $this->presenter["sufijo"], $this->presenter["nombre"], $this->presenter["entidad"], $this->presenter["oficina"], date("dmy"));
+        $remittance.= $this->generatePresenter($this->presenter["nif"], $this->presenter["suffix"], $this->presenter["name"], $this->presenter["bank"], $this->presenter["branch"], date("dmy"));
 
         foreach ($this->applicants as $k => $v) {
-            $remesa.= $this->_generaOrdenante($v["nif"], $v["sufijo"], $v["nombre"], $v["entidad"], $v["oficina"], $v["dc"], $v["cuenta"], date("dmy"), date("dmy"), $k + 1);
+            $remittance.= $this->generateApplicante($v["nif"], $v["suffix"], $v["name"], $v["bank"], $v["branch"], $v["cd"], $v["account"], date("dmy"), date("dmy"), $k + 1);
             foreach ($this->individual[$k] as $r) {
-                $remesa.= $this->_generaIndividual($v["nif"], $v["sufijo"], $r["ref"], $r["nombre"], $r["entidad"], $r["oficina"], $r["dc"], $r["cuenta"], $this->_convierteImporte($r["importe"]), $r["concepto"]);
+                $remittance.= $this->generateIndividual($v["nif"], $v["suffix"], $r["ref"], $r["name"], $r["bank"], $r["branch"], $r["cd"], $r["account"], $this->convertAmount($r["amount"]), $r["concept"]);
 
-                $this->applicants[$k]["nRec"] ++;
-                $this->presenter["nRec"] ++;
-                $this->applicants[$k]["nReg"] ++;
-                $this->presenter["nReg"] ++;
-                $this->applicants[$k]["total"]+=$r["importe"];
-                $this->presenter["total"]+=$r["importe"];
+                $this->applicants[$k]["recNumber"] ++;
+                $this->presenter["recNumber"] ++;
+                $this->applicants[$k]["regNumber"] ++;
+                $this->presenter["regNumber"] ++;
+                $this->applicants[$k]["total"]+=$r["amount"];
+                $this->presenter["total"]+=$r["amount"];
             }
-            $remesa.= $this->_generaTotalOrdenante($v["nif"], $v["sufijo"], $this->_convierteImporte($this->applicants[$k]["total"]), $this->applicants[$k]["nRec"], $this->applicants[$k]["nReg"]);
-            $this->presenter["nOrd"] ++;
+            $remittance.= $this->generateTotalOrdenante($v["nif"], $v["suffix"], $this->convertAmount($this->applicants[$k]["total"]), $this->applicants[$k]["recNumber"], $this->applicants[$k]["regNumber"]);
+            $this->presenter["ordNumber"] ++;
         }
-        $this->presenter["nReg"]+=$this->presenter["nOrd"] * 2;
+        $this->presenter["regNumber"]+=$this->presenter["ordNumber"] * 2;
 
-        $remesa.= $this->_generaTotalGeneral($this->presenter["nif"], $this->presenter["sufijo"], $this->_convierteImporte($this->presenter["total"]), $this->presenter["nOrd"], $this->presenter["nRec"], $this->presenter["nReg"]);
+        $remittance.= $this->generateGeneralTotal($this->presenter["nif"], $this->presenter["suffix"], $this->convertAmount($this->presenter["total"]), $this->presenter["ordNumber"], $this->presenter["recNumber"], $this->presenter["regNumber"]);
 
-        return $remesa;
+        return $remittance;
     }
 
-    // Funciones privadas de la Clase
-
-    function _generaTotalGeneral($nif, $sufijo, $importe, $nOrd, $nRec, $nReg)
+    private function generateGeneralTotal($nif, $sufijo, $importe, $nOrd, $nRec, $nReg)
     {
         $texto = "5980";
-        $texto.= $this->_rellenaIzquierda($nif, " ", 9);
-        $texto.= $this->_rellenaIzquierda($sufijo, 0, 3);
-        $texto.= $this->_rellenaIzquierda("", " ", 12);
-        $texto.= $this->_rellenaIzquierda("", " ", 40);
-        $texto.= $this->_rellenaIzquierda($nOrd, 0, 4);
-        $texto.= $this->_rellenaIzquierda("", " ", 16);
-        $texto.= $this->_rellenaIzquierda($importe, 0, 10);
-        $texto.= $this->_rellenaIzquierda("", " ", 6);
-        $texto.= $this->_rellenaIzquierda($nRec, 0, 10);
-        $texto.= $this->_rellenaIzquierda($nReg, 0, 10);
-        $texto.= $this->_rellenaIzquierda("", " ", 20);
-        $texto.= $this->_rellenaIzquierda("", " ", 18);
+        $texto.= $this->fillLeft($nif, " ", 9);
+        $texto.= $this->fillLeft($sufijo, 0, 3);
+        $texto.= $this->fillLeft("", " ", 12);
+        $texto.= $this->fillLeft("", " ", 40);
+        $texto.= $this->fillLeft($nOrd, 0, 4);
+        $texto.= $this->fillLeft("", " ", 16);
+        $texto.= $this->fillLeft($importe, 0, 10);
+        $texto.= $this->fillLeft("", " ", 6);
+        $texto.= $this->fillLeft($nRec, 0, 10);
+        $texto.= $this->fillLeft($nReg, 0, 10);
+        $texto.= $this->fillLeft("", " ", 20);
+        $texto.= $this->fillLeft("", " ", 18);
         $texto.= "\r\n";
 
         return $texto;
     }
 
-    function _generaTotalOrdenante($nif, $sufijo, $importe, $nRec, $nReg)
+    private function generateTotalOrdenante($nif, $sufijo, $importe, $nRec, $nReg)
     {
         $texto = "5880";
-        $texto.= $this->_rellenaIzquierda($nif, " ", 9);
-        $texto.= $this->_rellenaIzquierda($sufijo, 0, 3);
-        $texto.= $this->_rellenaIzquierda("", " ", 12);
-        $texto.= $this->_rellenaIzquierda("", " ", 40);
-        $texto.= $this->_rellenaIzquierda("", " ", 20);
-        $texto.= $this->_rellenaIzquierda($importe, 0, 10);
-        $texto.= $this->_rellenaIzquierda("", " ", 6);
-        $texto.= $this->_rellenaIzquierda($nRec, 0, 10);
-        $texto.= $this->_rellenaIzquierda($nReg, 0, 10);
-        $texto.= $this->_rellenaIzquierda("", " ", 20);
-        $texto.= $this->_rellenaIzquierda("", " ", 18);
+        $texto.= $this->fillLeft($nif, " ", 9);
+        $texto.= $this->fillLeft($sufijo, 0, 3);
+        $texto.= $this->fillLeft("", " ", 12);
+        $texto.= $this->fillLeft("", " ", 40);
+        $texto.= $this->fillLeft("", " ", 20);
+        $texto.= $this->fillLeft($importe, 0, 10);
+        $texto.= $this->fillLeft("", " ", 6);
+        $texto.= $this->fillLeft($nRec, 0, 10);
+        $texto.= $this->fillLeft($nReg, 0, 10);
+        $texto.= $this->fillLeft("", " ", 20);
+        $texto.= $this->fillLeft("", " ", 18);
         $texto.= "\r\n";
 
         return $texto;
     }
 
-    function _generaIndividual($nif, $sufijo, $ref, $nombre, $entidad, $oficina, $dc, $cuenta, $importe, $concepto)
+    private function generateIndividual($nif, $sufijo, $ref, $nombre, $entidad, $oficina, $dc, $cuenta, $importe, $concepto)
     {
         $texto = "5680";
-        $texto.= $this->_rellenaIzquierda($nif, " ", 9);
-        $texto.= $this->_rellenaIzquierda($sufijo, 0, 3);
-        $texto.= $this->_rellenaIzquierda($ref, 0, 12);
-        $texto.= $this->_rellenaDerecha($nombre, " ", 40);
-        $texto.= $this->_rellenaIzquierda($entidad, " ", 4);
-        $texto.= $this->_rellenaIzquierda($oficina, " ", 4);
-        $texto.= $this->_rellenaIzquierda($dc, 0, 2);
-        $texto.= $this->_rellenaIzquierda($cuenta, 0, 10);
-        $texto.= $this->_rellenaIzquierda($importe, 0, 10);
-        $texto.= $this->_rellenaIzquierda("", " ", 16);
-        $texto.= $this->_rellenaDerecha($concepto, " ", 40);
-        $texto.= $this->_rellenaIzquierda("", " ", 8);
+        $texto.= $this->fillLeft($nif, " ", 9);
+        $texto.= $this->fillLeft($sufijo, 0, 3);
+        $texto.= $this->fillLeft($ref, 0, 12);
+        $texto.= $this->fillRight($nombre, " ", 40);
+        $texto.= $this->fillLeft($entidad, " ", 4);
+        $texto.= $this->fillLeft($oficina, " ", 4);
+        $texto.= $this->fillLeft($dc, 0, 2);
+        $texto.= $this->fillLeft($cuenta, 0, 10);
+        $texto.= $this->fillLeft($importe, 0, 10);
+        $texto.= $this->fillLeft("", " ", 16);
+        $texto.= $this->fillRight($concepto, " ", 40);
+        $texto.= $this->fillLeft("", " ", 8);
         $texto.= "\r\n";
 
         return $texto;
     }
 
-    function _generaPresentador($nif, $sufijo, $nombre, $entidad, $oficina, $fecha)
+    private function generatePresenter($nif, $sufijo, $nombre, $entidad, $oficina, $fecha)
     {
         $texto = "5180";
-        $texto.= $this->_rellenaIzquierda($nif, " ", 9);
-        $texto.= $this->_rellenaIzquierda($sufijo, 0, 3);
-        $texto.= $this->_rellenaIzquierda($fecha, " ", 6);
-        $texto.= $this->_rellenaIzquierda("", " ", 6);
-        $texto.= $this->_rellenaDerecha($nombre, " ", 40);
-        $texto.= $this->_rellenaIzquierda("", " ", 20);
-        $texto.= $this->_rellenaIzquierda($entidad, " ", 4);
-        $texto.= $this->_rellenaIzquierda($oficina, " ", 4);
-        $texto.= $this->_rellenaIzquierda("", " ", 12);
-        $texto.= $this->_rellenaIzquierda("", " ", 40);
-        $texto.= $this->_rellenaIzquierda("", " ", 14);
+        $texto.= $this->fillLeft($nif, " ", 9);
+        $texto.= $this->fillLeft($sufijo, 0, 3);
+        $texto.= $this->fillLeft($fecha, " ", 6);
+        $texto.= $this->fillLeft("", " ", 6);
+        $texto.= $this->fillRight($nombre, " ", 40);
+        $texto.= $this->fillLeft("", " ", 20);
+        $texto.= $this->fillLeft($entidad, " ", 4);
+        $texto.= $this->fillLeft($oficina, " ", 4);
+        $texto.= $this->fillLeft("", " ", 12);
+        $texto.= $this->fillLeft("", " ", 40);
+        $texto.= $this->fillLeft("", " ", 14);
         $texto.= "\r\n";
 
         return $texto;
     }
 
-    function _generaOrdenante($nif, $sufijo, $nombre, $entidad, $oficina, $dc, $cuenta, $fecha, $fechaCargo, $procedimiento)
+    private function generateApplicante($nif, $sufijo, $nombre, $entidad, $oficina, $dc, $cuenta, $fecha, $fechaCargo, $procedimiento)
     {
         $texto = "5380";
-        $texto.= $this->_rellenaIzquierda($nif, " ", 9);
-        $texto.= $this->_rellenaIzquierda($sufijo, 0, 3);
-        $texto.= $this->_rellenaIzquierda($fecha, " ", 6);
-        $texto.= $this->_rellenaIzquierda($fechaCargo, " ", 6);
-        $texto.= $this->_rellenaDerecha($nombre, " ", 40);
-        $texto.= $this->_rellenaIzquierda($entidad, " ", 4);
-        $texto.= $this->_rellenaIzquierda($oficina, " ", 4);
-        $texto.= $this->_rellenaIzquierda($dc, 0, 2);
-        $texto.= $this->_rellenaIzquierda($cuenta, 0, 10);
-        $texto.= $this->_rellenaIzquierda("", " ", 8);
-        $texto.= $this->_rellenaIzquierda($procedimiento, 0, 2);
-        $texto.= $this->_rellenaIzquierda("", " ", 10);
-        $texto.= $this->_rellenaIzquierda("", " ", 40);
-        $texto.= $this->_rellenaIzquierda("", " ", 14);
+        $texto.= $this->fillLeft($nif, " ", 9);
+        $texto.= $this->fillLeft($sufijo, 0, 3);
+        $texto.= $this->fillLeft($fecha, " ", 6);
+        $texto.= $this->fillLeft($fechaCargo, " ", 6);
+        $texto.= $this->fillRight($nombre, " ", 40);
+        $texto.= $this->fillLeft($entidad, " ", 4);
+        $texto.= $this->fillLeft($oficina, " ", 4);
+        $texto.= $this->fillLeft($dc, 0, 2);
+        $texto.= $this->fillLeft($cuenta, 0, 10);
+        $texto.= $this->fillLeft("", " ", 8);
+        $texto.= $this->fillLeft($procedimiento, 0, 2);
+        $texto.= $this->fillLeft("", " ", 10);
+        $texto.= $this->fillLeft("", " ", 40);
+        $texto.= $this->fillLeft("", " ", 14);
         $texto.= "\r\n";
 
         return $texto;
     }
 
-    function _convierteImporte($importe)
+    private function convertAmount($importe)
     {
         return number_format($importe, 2, "", "");
     }
 
-    function _rellenaDerecha($texto, $caracter, $longitud, $cortar = true)
+    private function fillRight($texto, $caracter, $longitud, $cortar = true)
     {
-        if ($cortar)
+        if ($cortar) {
             $texto = substr($texto, 0, $longitud);
+        }
 
-        for ($i = strlen($texto); $i < $longitud; $i++)
+        for ($i = strlen($texto); $i < $longitud; $i++) {
             $texto = $texto . $caracter;
+        }
 
         return $texto;
     }
 
-    function _rellenaIzquierda($texto, $caracter, $longitud, $cortar = true)
+    private function fillLeft($texto, $caracter, $longitud, $cortar = true)
     {
-        if ($cortar)
+        if ($cortar) {
             $texto = substr($texto, 0, $longitud);
+        }
 
-        for ($i = strlen($texto); $i < $longitud; $i++)
+        for ($i = strlen($texto); $i < $longitud; $i++) {
             $texto = $caracter . $texto;
+        }
 
         return $texto;
     }
